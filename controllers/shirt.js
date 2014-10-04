ba81b288c7c273adc029d5d2ebd983800fab93c1
Tim Watrous <twatrous@gmail.com>
2014-10-04 22:53:14 +0200

    sort and started addshirt post

--- controllers/shirt.js
+++ controllers/shirt.js
@@ -3,11 +3,18 @@ var Shirt = require('../models/Shirt');
  * GET /shirts
  * shirts page
  */
-
-
 exports.getShirts = function(req, res) {
-  Shirt.find({}, function(err, shirt) {
-    if (err) return next(err);
+  Shirt.find( { $query: {}, $orderby: { title : 1 } },  function(err, shirt) {
+    if (err) return err;
     res.render('shirts', {title : 'Shirts', shirts : shirt})
   });
 };
+
+
+/**
+ * POST new shirt for auth'd user
+ *
+ */
+exports.addShirt = function(req, res) {
+  Shirt.find
+}
