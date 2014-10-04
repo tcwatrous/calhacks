d34c658999a251bbd7e407548df1cbbb3e5cccf1
Kevin Park <kevinsm329@me.com>
2014-10-04 01:57:23 -0700

    update 1.2
    - toggle img bw

--- public/js/main.js
+++ public/js/main.js
@@ -2,13 +2,8 @@ $(document).ready(function() {
 
   // Place JavaScript code here...
 
-function swapImage(id,primary,secondary) {
-    src=document.getElementById(id).src;
-    if (src.match(primary)) {
-      document.getElementById(id).src=secondary;
-    } else {
-      document.getElementById(id).src=primary;
-    }
-  }
+$('.toggle').on('click', function() {
+  $(this).toggleClass('bw');
+ });
 
 });
