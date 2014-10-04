39216544cb3bff1b90dda985718a301e4a76a752
Kevin Park <kevinsm329@me.com>
2014-10-04 01:09:20 -0700

    update 1.2
    - swapImage.

--- public/js/main.js
+++ public/js/main.js
@@ -2,4 +2,13 @@ $(document).ready(function() {
 
   // Place JavaScript code here...
 
+function swapImage(id,primary,secondary) {
+    src=document.getElementById(id).src;
+    if (src.match(primary)) {
+      document.getElementById(id).src=secondary;
+    } else {
+      document.getElementById(id).src=primary;
+    }
+  }
+
 });
